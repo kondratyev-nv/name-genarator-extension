8383c6226a3500a73910845507f33415cc5029d8
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2016-06-18 09:07:04 +0600

    Add code generation for form fields

--- js/NameGeneratorExtensionForm.js
+++ js/NameGeneratorExtensionForm.js
@@ -1,70 +1,40 @@
 
 function NameGeneratorExtensionForm(document) {
     this.document = document;
+    this.valuesMap = {
+        firstName: 'firstname',
+        lastName: 'lastname',
+        email: 'email',
+        password: 'password' 
+    };
+    this.init();
 };
 
-NameGeneratorExtensionForm.prototype.firstName = function (name) {
-    if (name != null) {
-        this.getFirstNameField().value = name;
+NameGeneratorExtensionForm.prototype.init = function () {
+    for (var valueKey in this.valuesMap) {
+        this['get' + (valueKey.charAt(0).toUpperCase() + valueKey.slice(1)) + 'Field'] = this.createGetFieldFunction(valueKey);
+        this[valueKey] = this.createGetSetFunction(valueKey);
     }
-
-    return this.getFirstNameField().value;
 };
 
-NameGeneratorExtensionForm.prototype.lastName = function (name) {
-    if (name != null) {
-        this.getLastNameField().value = name;
-    }
+NameGeneratorExtensionForm.prototype.createGetFieldFunction = function (valueKey) {
+    return function () {
+        if (this[valueKey + 'Field'] == null) {
+            this[valueKey + 'Field'] = this.getElement(this.valuesMap[valueKey]);
+        }
 
-    return this.getLastNameField().value;
+        return this[valueKey + 'Field'];
+    };
 };
 
-NameGeneratorExtensionForm.prototype.email = function (name) {
-    if (name != null) {
-        this.getEmailField().value = name;
-    }
+NameGeneratorExtensionForm.prototype.createGetSetFunction = function (valueKey) {
+    return function (value) {
+        if (value != null) {
+            this['get' + (valueKey.charAt(0).toUpperCase() + valueKey.slice(1)) + 'Field']().value = value;
+        }
 
-    return this.getEmailField().value;
-};
-
-NameGeneratorExtensionForm.prototype.password = function (name) {
-    if (name != null) {
-        this.getPasswordField().value = name;
-    }
-
-    return this.getPasswordField().value;
-};
-
-NameGeneratorExtensionForm.prototype.getFirstNameField = function () {
-    if (this.firstNameField == null) {
-        this.firstNameField = this.getElement('firstname');
-    }
-
-    return this.firstNameField;
-};
-
-NameGeneratorExtensionForm.prototype.getLastNameField = function () {
-    if (this.lastNameField == null) {
-        this.lastNameField = this.getElement('lastname');
-    }
-
-    return this.lastNameField;
-};
-
-NameGeneratorExtensionForm.prototype.getEmailField = function () {
-    if (this.emailField == null) {
-        this.emailField = this.getElement('email');
-    }
-
-    return this.emailField;
-};
-
-NameGeneratorExtensionForm.prototype.getPasswordField = function () {
-    if (this.passwordField == null) {
-        this.passwordField = this.getElement('password');
-    }
-
-    return this.passwordField;
+        return this['get' + (valueKey.charAt(0).toUpperCase() + valueKey.slice(1)) + 'Field']().value;
+    };
 };
 
 NameGeneratorExtensionForm.prototype.getGeneratorOption = function () {
