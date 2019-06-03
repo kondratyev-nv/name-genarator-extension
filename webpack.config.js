030e3362aa8a20650f92f1756e245530110fecaf
Nikolay Kondratyev <kondratyev.nv@gmail.com>
2019-06-03 10:49:26 +0300

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
@@ -49,7 +50,8 @@ module.exports = {
         ]
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
         new CopyWebpackPlugin([
             'src/images',
             'src/manifest.json'
