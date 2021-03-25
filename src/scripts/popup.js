b942d307818e3d58494ae6f13323261c021b8ce7
Nikolay Kondratyev <4085884+kondratyev-nv@users.noreply.github.com>
2021-03-25 17:52:53 +0000

    Fix line endings

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -16,35 +16,35 @@ $(function () {
     var nameGenerator = new NameGeneratorExtension(document, [
         new FakerGenerator(),
         new RandomUserGenerator(),
+        new RandomProfileGenerator(),
         new UiNamesGenerator(),
-        new RandomProfileGenerator()
     ]);
 
-    $('#refreshbtn').on("click", function () {
+    $('#refreshbtn').on('click', function () {
         nameGenerator.refresh();
     });
 
-    $('#savebtn').on("click", function () {
+    $('#savebtn').on('click', function () {
         nameGenerator.save();
     });
 
-    $('#loadbtn').on("click", function () {
+    $('#loadbtn').on('click', function () {
         nameGenerator.load();
     });
 
-    $('#removebtn').on("click", function () {
+    $('#removebtn').on('click', function () {
         nameGenerator.remove();
     });
 
-    $('#generatortype').on("change", function () {
+    $('#generatortype').on('change', function () {
         nameGenerator.changeGenerator(true);
     });
 
-    $('#error-message .close').on("click", function () {
+    $('#error-message .close').on('click', function () {
         $('#error-message').hide();
     });
 
-    $('body').on("click", 'a', function () {
+    $('body').on('click', 'a', function () {
         if ($(this).attr('action-in') != 'popup') {
             chrome.tabs.create({ url: $(this).attr('href') });
         }
